c99cae33fbccfe4559432f2eedcfc47b836af81b
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-03-31 17:21:10 +0200

    Remove generated test and function

--- lib/brittle_ex_unit.ex
+++ lib/brittle_ex_unit.ex
@@ -1,18 +1,2 @@
 defmodule BrittleExUnit do
-  @moduledoc """
-  Documentation for BrittleExUnit.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BrittleExUnit.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/brittle_ex_unit_test.exs
+++ test/brittle_ex_unit_test.exs
@@ -1,8 +1,3 @@
 defmodule BrittleExUnitTest do
   use ExUnit.Case
-  doctest BrittleExUnit
-
-  test "greets the world" do
-    assert BrittleExUnit.hello() == :world
-  end
 end

33fb1e604bc53e5d43c1cfb3461a79e94f9ebdf4
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-04-26 09:19:44 +0200

    Brittle.ExUnit counts tests and failures

--- lib/brittle_ex_unit.ex
+++ lib/brittle_ex_unit.ex
@@ -1,6 +1,13 @@
 defmodule Brittle.ExUnit do
   def init(_) do
-    {:ok, %{test_count: 0}}
+    {:ok, %{test_count: 0, failure_count: 0}}
+  end
+
+  def handle_cast({:test_finished, %ExUnit.Test{state: {:failed, _}}}, state) do
+    {
+      :noreply,
+      %{state | test_count: state.test_count + 1, failure_count: state.failure_count + 1}
+    }
   end
 
   def handle_cast({:test_finished, _}, state) do
--- test/brittle_ex_unit_test.exs
+++ test/brittle_ex_unit_test.exs
@@ -1,11 +1,12 @@
 defmodule Brittle.ExUnitTest do
   use ExUnit.Case
 
-  test "counts tests" do
+  test "counts tests and failures" do
     {:ok, pid} = GenServer.start_link(Brittle.ExUnit, [])
 
     GenServer.cast(pid, {:test_finished, %ExUnit.Test{}})
+    GenServer.cast(pid, {:test_finished, %ExUnit.Test{state: {:failed, []}}})
 
-    assert :sys.get_state(pid) == %{test_count: 1}
+    assert :sys.get_state(pid) == %{test_count: 2, failure_count: 1}
   end
 end

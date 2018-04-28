29473f19f1d8576a0e71dfc51cefa21f44704f0b
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-04-28 13:49:13 +0200

    SystemMock doesn't rely on Agent to be running

--- test/support/system_mock.ex
+++ test/support/system_mock.ex
@@ -1,48 +1,51 @@
 defmodule SystemMock do
   def start_link do
-    Agent.start_link(
-      fn ->
-        %{
-          {"git", ~w(status --porcelain)} => " M lib/system_data.ex\n"
-        }
-      end,
-      name: __MODULE__
-    )
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   def set(mock, command, result) do
     Agent.update(mock, &Map.put(&1, command, result))
   end
 
-  def cmd("hostname", []) do
-    case System.cmd("hostname", []) do
-      {_, 0} -> {"Alices-MBP.fritz.box\n", 0}
+  def cmd("hostname" = cmd, [] = args) do
+    execute_command_and_replace_result(
+      {cmd, args},
+      "Alices-MBP.fritz.box\n"
+    )
+  end
+
+  def cmd("git" = cmd, ~w(rev-parse --abbrev-ref HEAD) = args) do
+    execute_command_and_replace_result(
+      {cmd, args},
+      "develop\n"
+    )
+  end
+
+  def cmd("git" = cmd, ~w(rev-parse HEAD) = args) do
+    execute_command_and_replace_result(
+      {cmd, args},
+      "7f8136915fe249efa47a21a89ff0f04e880264fc\n"
+    )
+  end
+
+  def cmd("git" = cmd, ~w(status --porcelain) = args) do
+    execute_command_and_replace_result(
+      {cmd, args},
+      " M lib/system_data.ex\n"
+    )
+  end
+
+  defp execute_command_and_replace_result({cmd, args} = command, replacement) do
+    case System.cmd(cmd, args) do
+      {_, 0} -> {get(command) || replacement, 0}
       result -> result
     end
   end
 
-  def cmd("git", ~w(rev-parse --abbrev-ref HEAD)) do
-    case System.cmd("git", ~w(rev-parse --abbrev-ref HEAD)) do
-      {_, 0} -> {"develop\n", 0}
-      result -> result
-    end
-  end
-
-  def cmd("git", ~w(rev-parse HEAD)) do
-    case System.cmd("git", ~w(rev-parse HEAD)) do
-      {_, 0} -> {"7f8136915fe249efa47a21a89ff0f04e880264fc\n", 0}
-      result -> result
-    end
-  end
-
-  def cmd("git", ~w(status --porcelain)) do
-    case System.cmd("git", ~w(status --porcelain)) do
-      {_, 0} ->
-        {
-          Agent.get(__MODULE__, &Map.get(&1, {"git", ~w(status --porcelain)})),
-          0
-        }
-      result -> result
+  defp get(command) do
+    case Process.whereis(__MODULE__) do
+      nil -> nil
+      _ -> Agent.get(__MODULE__, &Map.get(&1, command))
     end
   end
 end

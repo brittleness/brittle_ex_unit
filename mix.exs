a0b0491400620401742d56942c50f51327309f95
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-12 11:50:39 +0200

    Relax elixir dependency to ~> 1.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Brittle.ExUnit.MixProject do
     [
       app: :brittle_ex_unit,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env),
       deps: deps()

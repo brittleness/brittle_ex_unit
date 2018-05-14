62ab2154400b3907ca94afe7a237c2324729b6af
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-14 20:51:59 +0200

    Only configure the payload directory and mocks in the test env

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,8 @@
 use Mix.Config
 
-config :brittle_ex_unit,
-  payload_directory: "_build/test/payloads/#{Enum.random(100..999)}",
-  date_time: DateTimeMock,
-  system: SystemMock
+if Mix.env() == :test do
+  config :brittle_ex_unit,
+    payload_directory: "_build/test/payloads/#{Enum.random(100..999)}",
+    date_time: DateTimeMock,
+    system: SystemMock
+end
